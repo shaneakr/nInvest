e020c38a521537d828bcb817a4f26b4e920692f0
Shanea Leven <shanea@dgraph.io>
2018-10-09 11:12:19 -0700

    new company, home, profile and friends pages in rails app

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get 'profile', to: 'pages#profile'
+  get 'company', to: 'pages#company'
+  get 'friends', to: 'pages#friends'
+end
