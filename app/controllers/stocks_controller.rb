29e1d46da8092b088b5dffcfe9995b15ce333007
Shanea Leven <shanea@dgraph.io>
2018-10-11 14:22:37 -0700

    added error handling for empty and incorrect strings

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,14 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render json: @stock
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render json: @stock
+      else flash[:danger] = 'You have entered an invalid symbol'
+      redirect_to portfolio_path
+      end
+    else flash[:danger]= 'You have entered empty stock'
+      redirect_to portfolio_path
+      end
     end
 end
